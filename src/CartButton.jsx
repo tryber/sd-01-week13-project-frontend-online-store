6ac7e06e9aa24c6d3ad736e493ed8d3fded32d14
antoniopaolillo <antonio_francisco_p@live.com>
2019-12-03 12:00:20 -0300

    criando pagina do carrinho-de-compras

---
+++ src/CartButton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './cartShopping.css';
+
+
+class CartButton extends React.Component {
+  render() {
+    return (
+      <div>
+        <Link to="/carrinho-de-compras">
+          <img
+            className="cartImage"
+            src={require("./icons/cart.jpg")}
+            alt="shopping cart"
+          />
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default CartButton;
---
+++ src/CartShopping.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import './cartShopping.css';
+
+class CartShopping extends React.Component {
+  validatingCart() {
+    if (this.props.products === 0 || this.props.products === undefined) {
+      return (
+        <div>
+          <img className="emptyBoxImage" src={require("./icons/emptyBox.png")} alt="empty box" />
+          <p>Seu carrinho está vazio</p>
+        </div>
+      );
+    } else {
+      return <p>teste</p>;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Link to="/">
+            <img className="backPageImage space" src={require("./icons/back.svg")} alt="back"/>
+          </Link>
+          <div className="cartProductsHeader space">
+            <img className="cartImage" src={require("./icons/cart.jpg")} alt="shopping cart"/>
+            <p><strong>Carrinho de Compras</strong></p>
+          </div>
+        </header>
+        <div className="emptyBoxContainer">
+            {this.validatingCart()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CartShopping;
