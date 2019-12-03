4d3e053097ff776568698197424efe85ff6c8508
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-12-03 12:30:06 -0200

    Mostrar Categorias

--- src/components/CategoryBar.js
+++ src/components/CategoryBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as workAPI from '../services/workAPI';
 
+
 export default class CategoryBar extends Component {
   constructor(props) {
     super(props);
@@ -10,22 +11,35 @@ export default class CategoryBar extends Component {
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     workAPI.getAPI('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((data) => this.setState({data}))
+      .then((data) => this.setState({ data }))
+  }
+
+  renderCategory(id, value, handleChange) {
+    return ( 
+      <label>
+        <input type='checkbox' key={id} value={value} onChange={handleChange} />
+        {value}
+      </label>
+      
+    );
+  }
+
+  handleChange(event) {
+    console.log(event.target.value)
   }
 
   render() {
     return (
       <div>
-
-      </div>
+        <h3>Categorias:</h3>
+        {this.state.data.map((data) => this.renderCategory(data.id, data.name, this.handleChange))}
+        </div>
     );
   }
 }
 
 
-CategoryBar.propTypes = {
 
-};
 
