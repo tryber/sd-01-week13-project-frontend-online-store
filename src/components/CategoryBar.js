58aef8ffbda5f022873ade10f88d11fc0c6c0da0
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-12-03 11:24:05 -0200

    Add o Escopo do Category Bar

---
+++ src/components/CategoryBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class CategoryBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+  
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+CategoryBar.propTypes = {
+
+};
