9a8ff93e50bb52fb10c289a8c498e00f7990252a
antoniopaolillo <antonio_francisco_p@live.com>
2019-12-05 17:09:54 -0300

    tentando arrumar codeClimate

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,29 +3,36 @@ import { Link } from 'react-router-dom';
 import Back from '../icons/back.svg';
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.showHeader = this.showHeader.bind(this);
+  }
+  showHeader() {
+    return (
+      <header>
+        <Link to="/">
+          <img className="backPageImage space" src={Back} alt="back" />
+        </Link>
+      </header>
+    );
+  }
   render() {
     const newResult = JSON.parse(localStorage.result);
-    const { title, price, thumbnail, attributes} = newResult;
+    const { title, price, thumbnail, attributes } = newResult;
     return (
       <div>
-        <header>
-          <Link to="/">
-            <img
-              className="backPageImage space"
-              src={Back}
-              alt="back"
-            />
-          </Link>
-        </header>
+        {this.showHeader()}
         <div>
           <p>{`Produto ${title} - R$${price}`}</p>
         </div>
         <div>
           <img src={thumbnail} alt="product" />
           <div>
-            <p><strong>Especificações Técnicas</strong></p>
+            <p>
+              <strong>Especificações Técnicas</strong>
+            </p>
             <ul>
-              {attributes.map(specification => (
+              {attributes.map((specification) => (
                 <li key={specification.name}>
                   {specification.name} : {specification.value_name}
                 </li>
