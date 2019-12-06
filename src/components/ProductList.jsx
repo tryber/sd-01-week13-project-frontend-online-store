a8feb8efab6ca242c5674d958a599b3116ea6cef
Richard Wellerson <richardwellerson@gmail.com>
2019-12-06 16:09:49 -0300

    Botoes Criados

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -49,9 +49,10 @@ class ProductsList extends React.Component {
   render() {
     const { shouldUpdate, results } = this.state;
     const { searched, updateCartState } = this.props;
-    console.log(results)
     return (
       <div className="card-container">
+        <button>Ordenar por Maior</button>
+        <button>Ordenar por Menor</button>
         {shouldUpdate ? (
           <EachProduct results={results} searched={searched} onClick={updateCartState} />
         ) : (
