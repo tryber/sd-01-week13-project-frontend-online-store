b62452c30ae86be2e104bf5bf43b40c98de39ed0
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-12-07 13:23:18 -0200

    Code Climate 0.4

--- src/components/EmptyShoppingCar.jsx
+++ src/components/EmptyShoppingCar.jsx
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import EmptyBox from '../icons/emptyBox.png';
 
 export default class EmptyShoppingCar extends Component {
-
-    render() {
-        return (
-            <div className="cart-shopping-products">
-                <img className="cart-shopping-emptyimg" src={EmptyBox} alt="empty box" />
-                <p> <span> Seu carrinho está vazio </span> </p>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="cart-shopping-products">
+        <img className="cart-shopping-emptyimg" src={EmptyBox} alt="empty box" />
+        <p> <span> Seu carrinho está vazio </span> </p>
+      </div>
+    );
+  }
 }
