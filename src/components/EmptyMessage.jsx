052cb597763f3a016ec33c771bd0d8a017f487f7
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-12-07 13:15:15 -0200

    Code Climate 0.1

--- src/components/EmptyMessage.jsx
+++ src/components/EmptyMessage.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 export default class EmptyMessage extends Component {
-
   render() {
     return (
       <div className={'empty-message'}>
@@ -9,5 +8,4 @@ export default class EmptyMessage extends Component {
       </div>
     );
   }
-
 }
