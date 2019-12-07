362c465339a0d5d81e35b0aae604b0f2ca218892
Richard Wellerson <richardwellerson@gmail.com>
2019-12-07 13:09:59 -0300

    Inserindo componente button

---
+++ src/components/ButtonOrderedBy.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProductList from './ProductList';
+import EachProduct from './EachProduct';
+
+class ButtonOrderedBy extends React.Component {
+  constructor(props){
+    super(props);
+    
+  }
+}
