e293361a66fe1dd75264484735d8719d5427345b
Richard Wellerson <richardwellerson@gmail.com>
2019-12-07 15:55:38 -0300

    Refatorado para Option

--- src/components/ButtonOrderedBy.jsx
+++ src/components/ButtonOrderedBy.jsx
@@ -2,31 +2,43 @@ import React from 'react';
 
 class ButtonOrderedBy extends React.Component {
 
-
-  sortResultLessValue(){
-    const {result, onChange} = this.props;
+  sortResultLessValue() {
+    const { result, onChange } = this.props;
     const ordenedResult = result.slice(0);
-    ordenedResult.sort(function(a,b) {
-        return a.price - b.price;
+    ordenedResult.sort(function (a, b) {
+      return a.price - b.price;
     });
     onChange(ordenedResult);
   }
 
-  sortResultBiggerValue(){
-    const {result, onChange} = this.props;
+  sortResultBiggerValue() {
+    const { result, onChange } = this.props;
     const ordenedResult = result.slice(0);
-    ordenedResult.sort(function(a,b) {
-        return b.price - a.price;
+    ordenedResult.sort(function (a, b) {
+      return b.price - a.price;
     });
     onChange(ordenedResult);
+  }
+
+  findValueSelect(event) {
+    if (event.target.value === 'less') {
+      this.sortResultLessValue();
+    } else if (event.target.value === 'bigger') {
+      this.sortResultBiggerValue();
+    } else {
+      return '';
+    }
   }
 
   render() {
     return (
       <div>
-      <button onClick={() => this.sortResultLessValue()}>Ordenar por Menor Valor</button>
-      <button onClick={() => this.sortResultBiggerValue()}>Ordenar por Maior Valor</button>
-    </div>
+        <select onChange={(e) => this.findValueSelect(e)}>
+          <option>---Ordenar Por---</option>
+          <option value='less'>Menor Valor</option>
+          <option value='bigger'>Maior Valor</option>
+        </select>
+      </div>
     )
   }
 }
