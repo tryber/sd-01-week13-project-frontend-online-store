d77d287a12bccc682ec67d0ce0ad4fff073992bb
Richard Wellerson <richardwellerson@gmail.com>
2019-12-07 13:14:18 -0300

    Subindo class e constructor prontos

--- src/components/ButtonOrderedBy.jsx
+++ src/components/ButtonOrderedBy.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProductList from './ProductList';
+import { results } from '/ProductList';
 import EachProduct from './EachProduct';
 
 class ButtonOrderedBy extends React.Component {
   constructor(props){
     super(props);
-    
   }
 }
+
+export default ButtonOrderedBy;
