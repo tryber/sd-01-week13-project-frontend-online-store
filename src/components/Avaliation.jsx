cde1265ac6c47f505798155ae94cbce4ae2e7425
antoniopaolillo <antonio_francisco_p@live.com>
2019-12-07 20:19:16 -0300

    estilizando avaliation

--- src/components/Avaliation.jsx
+++ src/components/Avaliation.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Box from '@material-ui/core/Box';
-import Rating from '@material-ui/lab/Rating';
-import './avaliation.css';
+import React from "react";
+import PropTypes from "prop-types";
+import Box from "@material-ui/core/Box";
+import Rating from "@material-ui/lab/Rating";
+import "./avaliation.css";
 
 class Avaliation extends React.Component {
   static showComments(comment) {
     return (
-      <div>
-        <div>
-          <p>Email: {comment.email}</p>
+      <div class="container-comment">
+        <div class="elements-comment">
+          <div className="email-comment">
+            <p>{comment.email}</p>
+            <Box display="flex" flexDirection="column">
+              <Rating
+                name="size-medium"
+                value={comment.valueStar}
+                size="large"
+              />
+            </Box>
+          </div>
+          <div>
+            <p>{comment.textArea}</p>
+          </div>
         </div>
-        <div>
-          <p>Comentário: </p>
-          <p>{comment.textArea}</p>
-        </div>
-        <Box display="flex" flexDirection="column">
-          <Rating name="size-medium" value={comment.valueStar} size="large" />
-        </Box>
       </div>
     );
   }
@@ -38,22 +43,27 @@ class Avaliation extends React.Component {
     if (localStorage.comments) {
       const commentsFromLocalStorage = localStorage.comments;
       const formatedComment = JSON.parse(commentsFromLocalStorage);
-      const comments = formatedComment.filter((key) => key.id === id);
+      const comments = formatedComment.filter(key => key.id === id);
       return (
-        <div>{comments.map((comment) => Avaliation.showComments(comment))}</div>
+        <div>{comments.map(comment => Avaliation.showComments(comment))}</div>
       );
     }
     return <div>Nenhum comentário feito!</div>;
   }
 
   render() {
-    return <div>{this.findComments()}</div>;
+    return (
+      <fieldset>
+        <legend>Comentários</legend>
+        {this.findComments()}
+      </fieldset>
+    );
   }
 }
 
 Avaliation.propTypes = {
   id: PropTypes.string.isRequired,
-  update: PropTypes.string.isRequired,
+  update: PropTypes.string.isRequired
 };
 
 export default Avaliation;
