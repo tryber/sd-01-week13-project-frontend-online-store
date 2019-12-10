a8be3b28edb824e5c16598cc19c13ad8d92e69bf
Richard Wellerson <richardwellerson@gmail.com>
2019-12-10 15:33:11 -0300

    Code Climate

--- src/components/FormForFinishingShopping.jsx
+++ src/components/FormForFinishingShopping.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './finishingShopping.css';
 
 class FormForFinishingShopping extends React.Component {
-  
+
   componentDidUpdate(prevProps) {
     if (prevProps !== this.props) {
       return this.formPurchase();
     }
+    return '';
   }
 
   createInput(id, placeholder) {
@@ -49,3 +51,10 @@ class FormForFinishingShopping extends React.Component {
 }
 
 export default FormForFinishingShopping;
+
+FormForFinishingShopping.propTypes = {
+  changeHandler: PropTypes.func.isRequired,
+  state: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }),
+}
