159924ce0884d3772076fd3f65aafb3bae302cc9
Fernando Kenji Sakabe <fernandokenjisakabe@gmail.com>
2019-12-10 10:55:21 -0300

    adicionando erro

---
+++ src/arquivo.js
@@ -0,0 +1,15 @@
+for (let i = 10; i > 0; i += 1) {
+  console.log(i)
+}
+
+function soma1(a, b){
+    return a+b
+}
+
+function soma2(a, b){
+    return a+b
+}
+
+function soma3(a, b){
+    return a+b
+}
