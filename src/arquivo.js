c99f9204a12653e9d8a99235db84e6254879652a
Fernando Kenji Sakabe <fernandokenjisakabe@gmail.com>
2019-12-10 11:48:30 -0300

    tirando loop for

--- src/arquivo.js
+++ src/arquivo.js
@@ -1,15 +1,9 @@
-for (let i = 10; i > 0; i += 1) {
-  console.log(i)
-}
-
 function soma1(a, b){
-    return a+b
+  return a+b
 }
 
 function soma2(a, b){
-    return a+b
+  return a+b
 }
 
-function soma3(a, b){
-    return a+b
-}
+console.log(soma1(2, 4))
