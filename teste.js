13b3f391501d69d3d7ea76f2773e9090a789e182
Fernando Kenji Sakabe <fernandokenjisakabe@gmail.com>
2019-12-10 12:17:58 -0300

    Ädicionando funções

--- teste.js
+++ teste.js
@@ -1,11 +1,7 @@
-for( let i = 0; i < 10; i++ ){
-  console.log(i)
+function soma1(a, b){
+  return a + b
 }
 
-soma1(a, b){
-    return a + b
+function soma2(a, b){
+  return a + b
 }
-
-soma2(a, b){
-    return a + b
-  }
