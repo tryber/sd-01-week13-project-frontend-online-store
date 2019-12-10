e12e37f79be52b121b354cbb1c31447461a540cc
Richard Wellerson <richardwellerson@gmail.com>
2019-12-10 15:26:51 -0300

    Code Climate

--- src/components/FormForFinishingShopping.jsx
+++ src/components/FormForFinishingShopping.jsx
@@ -2,49 +2,50 @@ import React from 'react';
 import './finishingShopping.css';
 
 class FormForFinishingShopping extends React.Component {
-    formPurchase() {
-        return (
-          <form name="formPurchase">
-            {this.createInput('name', 'Nome Completo')}
-            {this.createInput('cpf', 'CPF')}
-            {this.createInput('email', 'Email')}
-            {this.createInput('cellphone', 'Telefone')}
-            {this.createInput('cep', 'CEP')}
-            {this.createInput('adress', 'Endereço')}
-            {this.createInput('complete', 'Complemento')}
-            {this.createInput('number', 'Número')}
-            {this.createInput('city', 'CIdade')}
-          </form>
-        );
-      }
-    
-      createInput(id, placeholder) {
-        const { changeHandler, state } = this.props;
-        return (
-          <input
-            type="text"
-            className={state[`${id}Validation`] ? 'valid' : 'invalid'}
-            onChange={(e) => changeHandler(e)}
-            id={id}
-            required
-            placeholder={placeholder}
-          />
-        );
-      }
 
-      componentDidUpdate(prevProps) {
-          if(prevProps !== this.props) {
-              return this.formPurchase();
-          }
-      }
+  createInput(id, placeholder) {
+    const { changeHandler, state } = this.props;
+    return (
+      <input
+        type="text"
+        className={state[`${id}Validation`] ? 'valid' : 'invalid'}
+        onChange={(e) => changeHandler(e)}
+        id={id}
+        required
+        placeholder={placeholder}
+      />
+    );
+  }
 
-    render() {
-        return (
-            <div>
-                {this.formPurchase()}
-            </div>
-        );
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      return this.formPurchase();
     }
+  }
+
+  formPurchase() {
+    return (
+      <form name="formPurchase">
+        {this.createInput('name', 'Nome Completo')}
+        {this.createInput('cpf', 'CPF')}
+        {this.createInput('email', 'Email')}
+        {this.createInput('cellphone', 'Telefone')}
+        {this.createInput('cep', 'CEP')}
+        {this.createInput('adress', 'Endereço')}
+        {this.createInput('complete', 'Complemento')}
+        {this.createInput('number', 'Número')}
+        {this.createInput('city', 'CIdade')}
+      </form>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.formPurchase()}
+      </div>
+    );
+  }
 }
 
 export default FormForFinishingShopping;
