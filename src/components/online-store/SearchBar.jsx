f51d10a0315133e1ea345c5638a2c0a6eddd3a34
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-12-16 01:20:03 -0200

    Render Search Bar

---
+++ src/components/online-store/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+export default class SearchBar extends Component {
+    render() {
+        return (
+            <div>SearchBar</div>
+        );
+    }
+}
--- src/pages/OnlineStore.jsx
+++ src/pages/OnlineStore.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './OnlineStore.css';
+import SearchBar from '../components/online-store/SearchBar';
 
 export default class OnlineStore extends Component {
     render() {
         return (
             <div className="online-store">
-
+                <header>
+                    <h1>Online Store</h1>
+                    <SearchBar />
+                </header>
             </div>
         );
     }
